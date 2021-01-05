27fae3e2c17d2a0ca0d1f448679b85126a2343c6
robot9001 <robo9k@symlink.io>
2021-01-05 17:08:19 +0100

    WIP `impl Decoder` for `getserversResponse` message

--- dpmaster-codec/src/lib.rs
+++ dpmaster-codec/src/lib.rs
@@ -1,6 +1,6 @@
 use bytes::{BufMut, BytesMut};
 use cookie_factory::gen;
-//use dpmaster_proto::deserializer::getserversresponse_message;
+use dpmaster_proto::deserializer::getserversresponse_message;
 use dpmaster_proto::messages::{GetServersMessage, GetServersResponseMessage};
 use dpmaster_proto::serializer::gen_getservers_message;
 use tokio_util::codec::{Decoder, Encoder};
@@ -12,7 +12,7 @@ impl Encoder<GetServersMessage> for GetServersMessageEncoder {
 
     fn encode(&mut self, item: GetServersMessage, dst: &mut BytesMut) -> Result<(), Self::Error> {
         gen(gen_getservers_message(&item), dst.writer())
-            .map_err(|e| std::io::Error::new(std::io::ErrorKind::Other, e))
+            .map_err(|e| std::io::Error::new(std::io::ErrorKind::Other, e)) // TODO
             .map(|_| ())
     }
 }
@@ -23,11 +23,10 @@ impl Decoder for GetServersResponseMessageDecoder {
     type Item = GetServersResponseMessage;
     type Error = std::io::Error;
 
-    fn decode(&mut self, _src: &mut BytesMut) -> Result<Option<Self::Item>, Self::Error> {
-        todo!();
-        //getserversresponse_message(src)
-        //    .map_err(|e| std::io::Error::new(std::io::ErrorKind::Other, e))
-        //    .map(|(_, m)| Some(m))
+    fn decode(&mut self, src: &mut BytesMut) -> Result<Option<Self::Item>, Self::Error> {
+        getserversresponse_message(&src[..])
+            .map_err(|_e| std::io::Error::new(std::io::ErrorKind::Other, "uhoh")) // TODO
+            .map(|(_, m)| Some(m))
     }
 }
 
