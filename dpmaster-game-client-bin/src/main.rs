aba075d5eaec29fbc0de1308589dda024d11d521
robot9001 <robo9k@symlink.io>
2021-01-06 17:15:44 +0100

    Better logging with `color-eyre` and `tracing`

--- dpmaster-game-client-bin/src/main.rs
+++ dpmaster-game-client-bin/src/main.rs
@@ -1,11 +1,14 @@
 use clap::Clap;
+use color_eyre::{eyre::Report, eyre::WrapErr};
 use dpmaster_codec::GameClientCodec;
 use dpmaster_proto::messages::{FilterOptions, GetServersMessage};
+use eyre::eyre;
 use futures::{FutureExt, SinkExt};
 use std::net::ToSocketAddrs;
 use tokio::net::UdpSocket;
 use tokio_stream::StreamExt;
 use tokio_util::udp::UdpFramed;
+use tracing::{debug, info};
 
 #[derive(Clap, Debug)]
 struct Opts {
@@ -43,24 +46,41 @@ struct GetServersOpts {
 }
 
 #[tokio::main]
-pub async fn main() -> Result<(), Box<dyn std::error::Error>> {
+pub async fn main() -> Result<(), Report> {
+    install_tracing();
+
+    color_eyre::install()?;
+
     let opts: Opts = Opts::parse();
-    println!("opts: {:?}", opts);
+    debug!(?opts, "Parsed CLI options");
 
     match opts.subcmd {
         SubCommand::GetServers(getservers_opts) => {
             let socket = UdpSocket::bind("0.0.0.0:0").await?;
-            println!("Bound socket to local address {:?}", socket.local_addr()?);
+            info!(local_addr = % socket.local_addr()?, "Bound UDP socket");
 
             let mut framed = UdpFramed::new(socket, GameClientCodec::new());
 
             let addr = getservers_opts
                 .master_server
                 .to_socket_addrs()
-                .unwrap()
+                .wrap_err_with(|| {
+                    format!(
+                        "Failed to resolve master server {}",
+                        getservers_opts.master_server
+                    )
+                })?
                 .next()
-                .unwrap();
-            println!("master server address: {:?}", addr);
+                .ok_or_else(|| {
+                    eyre!(
+                        "Master server {} does not resolve to any address",
+                        getservers_opts.master_server
+                    )
+                })?;
+            info!(
+                master_server = % addr,
+                "Resolved master server {}", getservers_opts.master_server
+            );
 
             let getservers = GetServersMessage::new(
                 Some(getservers_opts.game_name),
@@ -71,13 +91,30 @@ pub async fn main() -> Result<(), Box<dyn std::error::Error>> {
                     getservers_opts.full,
                 ),
             );
-            println!("Sending request {:?}", getservers);
+            info!(request = ? getservers, "Sending request");
             framed.send((getservers, addr)).await?;
 
-            let (getserversresponse, _addr) = framed.next().map(|e| e.unwrap()).await?;
-            println!("response: {:?}", getserversresponse);
+            let (getserversresponse, _addr) = framed.next().map(|e| e.unwrap()).await?; // TODO
+            info!(response = ? getserversresponse, "Recieved response");
         }
     }
 
     Ok(())
+}
+
+fn install_tracing() {
+    use tracing_error::ErrorLayer;
+    use tracing_subscriber::prelude::*;
+    use tracing_subscriber::{fmt, EnvFilter};
+
+    let fmt_layer = fmt::layer().with_target(false);
+    let filter_layer = EnvFilter::try_from_default_env()
+        .or_else(|_| EnvFilter::try_new("info"))
+        .unwrap();
+
+    tracing_subscriber::registry()
+        .with(filter_layer)
+        .with(fmt_layer)
+        .with(ErrorLayer::default())
+        .init();
 }
