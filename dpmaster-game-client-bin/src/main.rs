b69b06f396f116ad21e625ccf7ba93d99658e5e1
robot9001 <robo9k@symlink.io>
2021-01-06 14:41:33 +0100

    Use `clap` to parse CLI config

--- dpmaster-game-client-bin/src/main.rs
+++ dpmaster-game-client-bin/src/main.rs
@@ -1,3 +1,4 @@
+use clap::Clap;
 use dpmaster_codec::GameClientCodec;
 use dpmaster_proto::messages::{FilterOptions, GetServersMessage};
 use futures::{FutureExt, SinkExt};
@@ -6,30 +7,77 @@ use tokio::net::UdpSocket;
 use tokio_stream::StreamExt;
 use tokio_util::udp::UdpFramed;
 
+#[derive(Clap, Debug)]
+struct Opts {
+    #[clap(subcommand)]
+    subcmd: SubCommand,
+}
+
+#[derive(Clap, Debug)]
+enum SubCommand {
+    GetServers(GetServersOpts),
+}
+
+type Bytes = Vec<u8>;
+
+// TODO: local_bind_addr
+#[derive(Clap, Debug)]
+struct GetServersOpts {
+    #[clap(short, long)]
+    master_server: String,
+
+    #[clap(short = 'n', long, parse(from_str))]
+    game_name: Bytes,
+
+    #[clap(short, long)]
+    protocol_number: u32,
+
+    #[clap(short = 't', long, parse(from_str))]
+    game_type: Option<Bytes>,
+
+    #[clap(short, long)]
+    empty: bool,
+
+    #[clap(short, long)]
+    full: bool,
+}
+
 #[tokio::main]
 pub async fn main() -> Result<(), Box<dyn std::error::Error>> {
-    let socket = UdpSocket::bind("0.0.0.0:0").await?;
-    println!("Bound socket to local address {:?}", socket.local_addr()?);
+    let opts: Opts = Opts::parse();
+    println!("opts: {:?}", opts);
 
-    let mut framed = UdpFramed::new(socket, GameClientCodec::new());
+    match opts.subcmd {
+        SubCommand::GetServers(getservers_opts) => {
+            let socket = UdpSocket::bind("0.0.0.0:0").await?;
+            println!("Bound socket to local address {:?}", socket.local_addr()?);
 
-    let addr = "master.worldofpadman.com:27955"
-        .to_socket_addrs()
-        .unwrap()
-        .next()
-        .unwrap();
-    println!("master server address: {:?}", addr);
+            let mut framed = UdpFramed::new(socket, GameClientCodec::new());
 
-    let getservers = GetServersMessage::new(
-        Some(b"WorldofPadman".to_vec()),
-        71,
-        FilterOptions::new(None, true, true),
-    );
-    println!("Sending request {:?}", getservers);
-    framed.send((getservers, addr)).await?;
+            let addr = getservers_opts
+                .master_server
+                .to_socket_addrs()
+                .unwrap()
+                .next()
+                .unwrap();
+            println!("master server address: {:?}", addr);
 
-    let (getserversresponse, _addr) = framed.next().map(|e| e.unwrap()).await?;
-    println!("response: {:?}", getserversresponse);
+            let getservers = GetServersMessage::new(
+                Some(getservers_opts.game_name),
+                getservers_opts.protocol_number,
+                FilterOptions::new(
+                    getservers_opts.game_type,
+                    getservers_opts.empty,
+                    getservers_opts.full,
+                ),
+            );
+            println!("Sending request {:?}", getservers);
+            framed.send((getservers, addr)).await?;
+
+            let (getserversresponse, _addr) = framed.next().map(|e| e.unwrap()).await?;
+            println!("response: {:?}", getserversresponse);
+        }
+    }
 
     Ok(())
 }
